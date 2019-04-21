3c12ad38699cc55ad4a5c8aebcf5f9958c0d40e6
paranerd <paranerd.development@gmail.com>
2019-04-21 13:10:43 +0200

    Fixed file encryption/decryption (did read bytes instead of strings)

--- python/aes.py
+++ python/aes.py
@@ -12,176 +12,176 @@ from Crypto.Protocol import KDF
 from Crypto.Hash import SHA, SHA256, HMAC
 
 class My_AES():
-    def __init__(self):
-        self.blockSize = 16
-        self.keySize = 32 # 32 bytes for aes 256 bit
+	def __init__(self):
+		self.blockSize = 16
+		self.keySize = 32 # 32 bytes for aes 256 bit
 
-    def generate_key(self, secret, salt):
-        return KDF.PBKDF2(secret, salt, self.keySize, 2048)
+	def generate_key(self, secret, salt):
+		return KDF.PBKDF2(secret, salt, self.keySize, 2048)
 
-    def random_string(self, length):
-        return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(length))
+	def random_string(self, length):
+		return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(length))
 
-    def encrypt(self, plaintext="", secret="", sign=False, is_file=False):
-        # Check if plaintext was passed
-        if not plaintext:
-            if not is_file:
-                plaintext = input('Enter text to encrypt: ')
+	def encrypt(self, plaintext="", secret="", sign=False, is_file=False):
+		# Check if plaintext was passed
+		if not plaintext:
+			if not is_file:
+				plaintext = input('Enter text to encrypt: ')
 
-        # Check if secret was passed
-        if not secret:
-            secret = getpass.getpass('Enter secret: ')
+		# Check if secret was passed
+		if not secret:
+			secret = getpass.getpass('Enter secret: ')
 
-        # Generate IV
-        iv = Random.new().read(self.blockSize)
+		# Generate IV
+		iv = Random.new().read(self.blockSize)
 
-        # Generate salt
-        salt = Random.new().read(self.blockSize)
+		# Generate salt
+		salt = Random.new().read(self.blockSize)
 
-        # Generate key
-        key = self.generate_key(secret, salt)
+		# Generate key
+		key = self.generate_key(secret, salt)
 
-        # Encrypt
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        enc = cipher.encrypt(self.pkcs5_pad(plaintext))
+		# Encrypt
+		cipher = AES.new(key, AES.MODE_CBC, iv)
+		enc = cipher.encrypt(self.pkcs5_pad(plaintext))
 
-        # Encode
-        enc64 = self.base64_url_encode(iv + salt + enc)
+		# Encode
+		enc64 = self.base64_url_encode(iv + salt + enc)
 
-        # Sign
-        if (sign):
-            enc64 = enc64 + ":" + self.sign(enc64, key)
+		# Sign
+		if (sign):
+			enc64 = enc64 + ":" + self.sign(enc64, key)
 
-        return enc64
+		return enc64
 
-    def decrypt(self, encrypted64="", secret="", is_file=False):
-        if not encrypted64:
-            if is_file:
-                print("No data to decrypt")
-                return ""
-            else:
-                encrypted64 = input("Enter encrypted data: ")
+	def decrypt(self, encrypted64="", secret="", is_file=False):
+		if not encrypted64:
+			if is_file:
+				print("No data to decrypt")
+				return ""
+			else:
+				encrypted64 = input("Enter encrypted data: ")
 
-        # Check if secret was passed
-        if not secret:
-            secret = getpass.getpass('Enter secret: ')
+		# Check if secret was passed
+		if not secret:
+			secret = getpass.getpass('Enter secret: ')
 
-        # Separate payload from potential hmac
-        separated = encrypted64.strip().split(":")
+		# Separate payload from potential hmac
+		separated = encrypted64.strip().split(":")
 
-        # Extract HMAC if signed
-        hmac = separated[1] if len(separated) > 1 else ""
+		# Extract HMAC if signed
+		hmac = separated[1] if len(separated) > 1 else ""
 
-        # Decode
-        enc = self.base64_url_decode(separated[0])
+		# Decode
+		enc = self.base64_url_decode(separated[0])
 
-        # Extract IV
-        iv = enc[0:self.blockSize]
+		# Extract IV
+		iv = enc[0:self.blockSize]
 
-        # Extract salt
-        salt = enc[self.blockSize:self.blockSize + self.blockSize]
+		# Extract salt
+		salt = enc[self.blockSize:self.blockSize + self.blockSize]
 
-        # Extract ciphertext
-        ciphertext = enc[self.blockSize + self.blockSize:]
+		# Extract ciphertext
+		ciphertext = enc[self.blockSize + self.blockSize:]
 
-        # Generate key
-        key = self.generate_key(secret, salt)
+		# Generate key
+		key = self.generate_key(secret, salt)
 
-        if (hmac and not (self.sign(separated[0], key) == hmac)):
-            return None
+		if (hmac and not (self.sign(separated[0], key) == hmac)):
+			return None
 
-        # Decrypt
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        decrypted = cipher.decrypt(ciphertext)
+		# Decrypt
+		cipher = AES.new(key, AES.MODE_CBC, iv)
+		decrypted = cipher.decrypt(ciphertext)
 
-        # Unpad and format
-        return self.pkcs5_unpad(decrypted.decode('utf-8'))
+		# Unpad and format
+		return self.pkcs5_unpad(decrypted.decode('utf-8'))
 
-    def encrypt_file(self, path="", secret=""):
-        if not path:
-            path = input("Enter path: ")
+	def encrypt_file(self, path="", secret=""):
+		if not path:
+			path = input("Enter path: ")
 
-        if not secret:
-            secret = getpass.getpass('Enter secret: ')
+		if not secret:
+			secret = getpass.getpass('Enter secret: ')
 
-        with open(path, 'rb') as fo:
-            plaintext = fo.read()
-            enc = self.encrypt(plaintext, secret)
+		with open(path, 'r') as fo:
+			plaintext = fo.read()
+			enc = self.encrypt(plaintext, secret)
 
-        with open(path + ".enc", 'wb') as fo:
-            fo.write(enc)
+		with open(path + ".enc", 'w') as fo:
+			fo.write(enc)
 
-        return path + ".enc"
+		return path + ".enc"
 
-    def decrypt_file(self, path="", secret=""):
-        if not path:
-            path = input("Enter path: ")
+	def decrypt_file(self, path="", secret=""):
+		if not path:
+			path = input("Enter path: ")
 
-        if not secret:
-            secret = getpass.getpass('Enter secret: ')
+		if not secret:
+			secret = getpass.getpass('Enter secret: ')
 
-        # Read file
-        with open(path, 'rb') as fo:
-            ciphertext = fo.read()
+		# Read file
+		with open(path, 'r') as fo:
+			ciphertext = fo.read()
 
-        # Decrypt
-        plaintext = self.decrypt(ciphertext, secret, True)
+		# Decrypt
+		plaintext = self.decrypt(ciphertext, secret, True)
 
-        # Build destination path
-        destPath = path.rsplit('.enc', 1)[0]
+		# Build destination path
+		destPath = path.rsplit('.enc', 1)[0]
 
-        # Write file
-        with open(destPath, 'wb') as fo:
-            fo.write(plaintext)
+		# Write file
+		with open(destPath, 'w') as fo:
+			fo.write(plaintext)
 
-        return destPath
+		return destPath
 
-    def pkcs5_pad(self, s):
-        return s + (self.blockSize - len(s) % self.blockSize) * chr(self.blockSize - len(s) % self.blockSize)
+	def pkcs5_pad(self, s):
+		return s + (self.blockSize - len(s) % self.blockSize) * chr(self.blockSize - len(s) % self.blockSize)
 
-    def pkcs5_unpad(self, s):
-        return s[0:-ord(s[-1])]
+	def pkcs5_unpad(self, s):
+		return s[0:-ord(s[-1])]
 
-    def base64_url_encode(self, s):
-        return base64.b64encode(s, altchars=b'-_').decode('utf-8')
+	def base64_url_encode(self, s):
+		return base64.b64encode(s, altchars=b'-_').decode('utf-8')
 
-    def base64_url_decode(self, s):
-        return base64.b64decode(s.encode(), altchars=b'-_')
+	def base64_url_decode(self, s):
+		return base64.b64decode(s.encode(), altchars=b'-_')
 
-    def sign(self, str, key):
-        myhash = HMAC.new(key, digestmod=SHA256)
-        myhash.update(str)
-        return myhash.hexdigest()
+	def sign(self, str, key):
+		myhash = HMAC.new(key, digestmod=SHA256)
+		myhash.update(str)
+		return myhash.hexdigest()
 
 class Main():
-    @staticmethod
-    def menu():
-        crypt = My_AES()
-        print("--- Main Menu ---")
-        print("[1] Encrypt Text")
-        print("[2] Decrypt Text")
-        print("[3] Encrypt File")
-        print("[4] Decrypt File")
-        print("[5] Exit")
+	@staticmethod
+	def menu():
+		crypt = My_AES()
+		print("--- Main Menu ---")
+		print("[1] Encrypt Text")
+		print("[2] Decrypt Text")
+		print("[3] Encrypt File")
+		print("[4] Decrypt File")
+		print("[5] Exit")
 
-        selection = input("Select: ")
-        print
+		selection = input("Select: ")
+		print
 
-        if selection == '1':
-            print("\nResult: " + crypt.encrypt())
-        elif selection == '2':
-            print("\nResult: " + crypt.decrypt())
-        elif selection == '3':
-            print("\nResult: " + crypt.encrypt_file())
-        elif selection == '4':
-            print("\nResult: " + crypt.decrypt_file())
-        elif selection == '5':
-            return
-        else:
-            print("Illegal selection")
+		if selection == '1':
+			print("\nResult: " + crypt.encrypt())
+		elif selection == '2':
+			print("\nResult: " + crypt.decrypt())
+		elif selection == '3':
+			print("\nResult: " + crypt.encrypt_file())
+		elif selection == '4':
+			print("\nResult: " + crypt.decrypt_file())
+		elif selection == '5':
+			return
+		else:
+			print("Illegal selection")
 
-        print
+		print
 
-        return Main().menu()
+		return Main().menu()
 
 Main().menu()
